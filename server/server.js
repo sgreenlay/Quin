e585b3a8c665fecfb288f6b535b414605af061fd
Fravic <fravicf@gmail.com>
2012-11-29 15:21:37 -0800

    hello world

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
-
+var express = require('express');
 var opengraph = require('./opengraph');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000);
 
