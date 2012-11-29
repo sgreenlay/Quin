7a3deefaf6c40151d093c622cfb9d5b78276e0b2
Fravic <fravicf@gmail.com>
2012-11-29 15:58:05 -0800

    server stuff

--- server/server.js
+++ server/server.js
@@ -2,10 +2,33 @@ var express = require('express');
 var opengraph = require('./opengraph');
 
 var app = express();
+app.configure(function(){
+    app.use(express.static(__dirname + '/public'));
+});
+
+/* Helpers */
+
+var ageQuery = function(token, callback) {
+    var query = '';
+    opengraph.fql(query, token, function(res) {
+        if (res == null) {
+            error();
+        }
+        callback(res);
+    });
+}
+
+/* Routes */
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.render("./public/index.html");
+});
+
+app.get('/query', function(req, res){
+    ageQuery(req.query.token, function(data) {
+        res.json(data);
+    });
 });
 
 app.listen(3000);
-
+console.log("Server Started on port 3000");
