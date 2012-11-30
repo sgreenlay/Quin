eec35612f1777bb4d256b7716d061982c7263cf7
Scott Greenlay <sgreenlay@gmail.com>
2012-11-30 11:21:43 -0800

    added permissions

--- iphone/Pylon/PYLAppDelegate.m
+++ iphone/Pylon/PYLAppDelegate.m
@@ -13,6 +13,41 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [FBSession openActiveSessionWithAllowLoginUI:true];
+    
+    NSArray *permissions = [NSArray arrayWithObjects:
+                            @"friends_about_me",
+                            @"friends_actions.music",
+                            @"friends_actions.news",
+                            @"friends_actions.video",
+                            @"friends_activities",
+                            @"friends_birthday",
+                            @"friends_education_history",
+                            @"friends_events",
+                            @"friends_games_activity",
+                            @"friends_groups",
+                            @"friends_hometown",
+                            @"friends_interests",
+                            @"friends_likes",
+                            @"friends_location",
+                            @"friends_notes",
+                            @"friends_photos",
+                            @"friends_questions",
+                            @"friends_relationship_details",
+                            @"friends_relationships",
+                            @"friends_religion_politics",
+                            @"friends_status",
+                            @"friends_subscriptions",
+                            @"friends_videos",
+                            @"friends_website",
+                            @"friends_work_history",
+                            nil];
+    
+    [FBSession openActiveSessionWithReadPermissions:permissions allowLoginUI:true completionHandler:^(FBSession *session, FBSessionState status, NSError *error) {
+        if (error) {
+            NSLog(@"Failure");
+        }
+    }];
+    
     return YES;
 }
 
